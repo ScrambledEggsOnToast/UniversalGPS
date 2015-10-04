70388f9f3934fba31cb502abf7ae8271d1d43bb7
Josh Kirklin <jjvk2@cam.ac.uk>
2015-10-05 00:23:03 +0100

    more of previous

--- test/CMakeFiles/test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
